e4efd311102be2ce6f3700a3b5f75dd25cdb9c26
tyxla <tyxla@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-31 20:31:57 +0000

    Version 1.0.9: Now properly supporting maximum characters limit.
    git-svn-id: https://plugins.svn.wordpress.org/gravity-forms-multiple-form-instances/trunk@1234902 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gravityforms-multiple-form-instances.php
+++ gravityforms-multiple-form-instances.php
@@ -5,7 +5,7 @@
  * Description: Allows multiple instances of the same form to be run on a single page when using AJAX.
  * Author: tyxla
  * Author URI: https://github.com/tyxla
- * Version: 1.0.8
+ * Version: 1.0.9
  * License: GPL2
  * Requires at least: 3.0.1
  * Tested up to: 4.3
@@ -83,6 +83,7 @@ class Gravity_Forms_Multiple_Form_Instances {
 			"trigger('gform_post_conditional_logic', [" . $form['id'] . ","		=> "trigger('gform_post_conditional_logic', [" . $random_id . ",",
 			'gformShowPasswordStrength("input_' . $form['id'] . '_'				=> 'gformShowPasswordStrength("input_' . $random_id . '_',
 			"gformInitChosenFields('#input_" . $form['id'] . "_"				=> "gformInitChosenFields('#input_" . $random_id . "_",
+			"jQuery('#input_" . $form['id'] . "_"				 				=> "jQuery('#input_" . $random_id . "_",
 			$hidden_field                                        				=> "<input type='hidden' name='gform_random_id' value='" . $random_id . "' />" . $hidden_field,
 		);
 
